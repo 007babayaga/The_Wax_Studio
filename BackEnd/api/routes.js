532d4f8fef18ef2e1d1c9f8fa013c4f714858f6a
Rajat Pratap <chauhanrajat515@gmail.com>
2025-12-18 20:20:05 +0530

    Half of the Architecture made

---
+++ BackEnd/api/routes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const { authRouter } = require('./v1/Auth/routes');
+
+const apiRouter = express.Router();
+apiRouter.use('/auth',authRouter)
+
+module.exports={apiRouter}
