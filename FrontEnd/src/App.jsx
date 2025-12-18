320e453d7b0c97f5ca98cfcf78fe2ef4a5f219b0
Rajat Pratap <chauhanrajat515@gmail.com>
2025-12-18 17:31:27 +0530

    Basic Frontend setup done

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,7 +1,7 @@
 const App = ()=>{
   return(
     <>
-    <p className="text-4xl text-amber-400">App Development is in Progress......</p>
+    <p className="text-4xl text-amber-400">App Development is in Progress.....</p>
     </>
   )
 }
